0086d569608623219ff67bcbc5b6a4e43dcbeff8
Lyla Traylor <lyla_traylor@berkeley.edu>
2025-08-20 13:34:11 -0700

    Fix formatting.

--- newton.py
+++ newton.py
@@ -1,3 +1,4 @@
+# packages
 import numpy as np
 
 # initial values
@@ -10,17 +11,18 @@ def derivative (f, x, h):
     fp = (f(x+h) - f(x))/h
     return fp
 
+# newton's method
 def newton_method(f, x_0, TOL):
-    x = np.array([x_0]) # create initial vector for approximations
+    x = np.array([x_0])
     for t in np.arrange(0, N):
-        fp = derivative(f, x[t], h) #value for the first derivative at first guess
+        fp = derivative(f, x[t], h) 
         fpp = derivative(fp, x[t], h)
         x_new = x[t] - fp/fpp
         x = np.append(x, x_new)
         if abs(x[t+1] - x[t]) < TOL:
             myans = x[-1]
             break
-        if N == 100:
+        if N == 100: # gives answer if doesn't reach tolerance
             myans = x[-1]
             break
     return myans
