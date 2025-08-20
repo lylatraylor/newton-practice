b99d32414b76070be32b15fdba16ecb0d94ea4b8
Lyla Traylor <lyla_traylor@berkeley.edu>
2025-08-20 10:57:17 -0700

    updated method.

--- newton_practice.py
+++ newton_practice.py
@@ -2,20 +2,22 @@ import numpy as np
 
 TOL = 10**(-3)
 N = 100
+h = 0.01
 
-def first_derivative (f, x, h):
+def derivative (f, x, h):
     fp = (f(x+h) - f(x))/h
     return fp
 
-def newton_method(f, x0):
-    x = np.array([x0])
-    fp = diff(f, x)
-    fpp = diff(fp, x)
-    for t in ([0, N]):
-        x[t+1] = x[t] - (fp[t]/fpp[t])
-        x = np.append(x, x[t+1])
+def newton_method(f, x_0, TOL):
+    x = np.array([x_0])
+    for t in np.arrange([0, N]):
+        fp = derivative(f, x[t], h)
+        fpp = derivative(fp, x[t], h)
+        x_new = x[t] - fp/fpp
+        x = np.append(x, x_new)
         if abs(x[t+1] - x[t]) < TOL:
-            
-        
+            myans = x[-1]
+            break
+    return myans
         
     
