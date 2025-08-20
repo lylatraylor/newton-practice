ac79e6f4bbe5d74cc4b914505e010a9919411a7e
Lyla Traylor <lyla_traylor@berkeley.edu>
2025-08-20 13:57:44 -0700

    added doc string.

--- newton.py
+++ newton.py
@@ -6,25 +6,32 @@ TOL = 10**(-3)
 N = 100
 h = 0.001
 
-# get derivative
+# get derivatives
 def derivative (f, x, h):
-    fp = (f(x+h) - f(x))/h
-    return fp
+    return (f(x+h) - f(x))/h
+
+def derivative_2 (f, x, h):
+    return (f(x+2*h) - 2*f(x+h) + f(x))/ h*h
 
 # newton's method
 def newton_method(f, x_0, TOL):
     """ Run Newton's method to minimize a function.
 
-    Parameters:
+    Parameters
+    ----------
     x0: starting value
     f: function
     TOL: tolerance
+    
+    Returns
+    -------
+    value of x that minimizes the function.
     
     """
     x = np.array([x_0])
     for t in np.arrange(0, N):
         fp = derivative(f, x[t], h) 
-        fpp = derivative(fp, x[t], h)
+        fpp = derivative_2(f, x[t], h)
         x_new = x[t] - fp/fpp
         x = np.append(x, x_new)
         if abs(x[t+1] - x[t]) < TOL:
